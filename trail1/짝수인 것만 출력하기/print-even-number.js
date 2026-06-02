faea6f405057e6bcf913a462bc33f0abd72de4a0
Minchaez <rlawlsdl0730@gmail.com>
2026-06-02 15:54:35 +0900

    [Passed] 짝수인 것만 출력하기 (63ms, 10MB)

---
+++ trail1/짝수인 것만 출력하기/print-even-number.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const arr = input[1].split(" ").map(Number);
+
+const result = [];
+
+for (let i = 0; i < n; i++) {
+    if (arr[i] % 2 === 0) {
+        result.push(arr[i]);
+    }
+}
+
+console.log(result.join(" "));
